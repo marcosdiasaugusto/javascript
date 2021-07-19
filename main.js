1da72f51c943a3e29185e7ec46e3ed572f4c53a1
marcosdiasaugusto <33377915+marcosdiasaugusto@users.noreply.github.com>
2021-07-18 23:42:48 -0300

    Add files via upload
    programa principal

---
+++ main.js
@@ -0,0 +1,81 @@
+var d = new Date();
+// Sun Jul 18 2021 23:25:51 GMT-0300 (Horário Padrão de Brasília)
+console.log(d);
+console.log(d.getMonth()+1);
+console.log(d.getMinutes());
+console.log(d.getDay());
+console.log(d.getHours());
+
+
+
+
+
+
+/*
+var count;
+for(count=0; count <= 5; count++){
+    console.log(count);
+};
+*/
+
+/*
+var count = 0;
+while (count < 5){
+    console.log(count);
+    count++;
+};
+*/
+
+/*
+// var idade =18;
+var idade = prompt("Qual sua idade ?")
+if (idade > 18) {
+    console.log("maior de idade");
+} else {
+    console.log("menor de idade");
+};
+*/
+
+/*
+var frutas = [{nome: "maça", cor:"vermelha"},{nome: "uva", cor:"roxa"}];
+console.log(frutas);
+console.log(frutas[1].nome);
+*/
+/*
+var fruta = {nome: "maça", cor:"vermelha"};
+console.log(fruta);
+console.log(fruta.nome);
+*/
+
+/* */
+// var lista = ["maça", "pera", "laranja"];
+// console.log(lista);
+// lista.push("uva")
+// console.log(lista);
+// lista.pop();
+// console.log(lista);
+// console.log(lista.reverse());
+// console.log(lista);
+// console.log(lista.length);
+// console.log(lista.toString());
+// console.log(lista[0]);
+// console.log(lista.toString()[0]);
+// console.log(lista.join(" - "));
+
+/* */
+//var nome = "Marcos Dias"
+//var idade = 54
+//var idade2 = 55
+//var frase = "Japão é o melhor time do mundo."
+
+//console.log(nome);
+//console.log(idade+idade2);
+//console.log(frase.replace("Japão","Brasil"));
+//console.log(frase.toUpperCase());
+
+
+//var idade = "54"
+//var idade2="55"
+// alert('Bem vindo ' + nome);
+// alert(nome + " tem " + idade + " anos.");
+//alert(idade+idade2)
